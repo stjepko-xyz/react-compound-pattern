9932897fa0f7e91b40b80ea5e4c3b584ed2f6b71
stjepko-xyz <24295554+stjepko-xyz@users.noreply.github.com>
2025-10-18 17:15:11 +0200

    Init

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+// add Prettier support
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      // add Prettier rules
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+          trailingComma: 'es5',
+          bracketSpacing: true,
+          endOfLine: 'lf',
+        },
+      ],
+    },
+  },
+]);
